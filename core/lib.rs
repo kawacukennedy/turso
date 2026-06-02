10840d9f31f6c815e4166ed1a2d7ff53710698ef
PThorpe92 <preston@turso.tech>
2026-06-02 14:53:13 -0400

    update sync engine to use new lml3 format

--- core/lib.rs
+++ core/lib.rs
@@ -1992,7 +1992,6 @@ impl Database {
                 self.durable_storage.clone(),
                 None,
             )?;
-            self.mv_store.store(Some(mv_store.clone()));
             let mvcc_bootstrap_conn = self._connect(true, None, None)?;
             match mv_store.bootstrap(mvcc_bootstrap_conn.clone()) {
                 Ok(()) => {}
@@ -2002,6 +2001,7 @@ impl Database {
                 }
                 Err(error) => return Err(error),
             }
+            self.mv_store.store(Some(mv_store));
         } else {
             self.mv_store.store(None);
         }
