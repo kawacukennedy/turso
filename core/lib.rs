5dc11df726cb6c6be3d61910e5337b08a7954119
PThorpe92 <preston@turso.tech>
2026-06-15 15:52:00 -0400

    use sequence_watermark fn in sync engine

--- core/lib.rs
+++ core/lib.rs
@@ -1992,6 +1992,7 @@ impl Database {
                 self.durable_storage.clone(),
                 None,
             )?;
+            self.mv_store.store(Some(mv_store.clone()));
             let mvcc_bootstrap_conn = self._connect(true, None, None)?;
             match mv_store.bootstrap(mvcc_bootstrap_conn.clone()) {
                 Ok(()) => {}
@@ -2001,7 +2002,6 @@ impl Database {
                 }
                 Err(error) => return Err(error),
             }
-            self.mv_store.store(Some(mv_store));
         } else {
             self.mv_store.store(None);
         }
